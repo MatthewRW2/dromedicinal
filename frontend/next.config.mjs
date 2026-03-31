47df2d4dfc3096f9f7f460e828abf5fa7a5a754d
José Mateo López Cifuentes <josemateolopezcifuentes@gmail.com>
2026-03-31 16:39:36 -0500

    .

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  reactCompiler: true,
-  images: { unoptimized: true },
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
